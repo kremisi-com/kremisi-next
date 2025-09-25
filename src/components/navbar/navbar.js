8d3cb3af950f2e73c1364a87defc9f498bb35720
Napolitano Andrea <napolitanoandrea00@gmail.com>
2025-09-25 18:46:02 +0200

    Add responsive navbar and mobile logo
    Introduced separate desktop and mobile navbars with distinct layouts and logo images. Added new dark and light logo icons, updated CSS for responsive visibility, and implemented a hamburger menu icon for mobile view.

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -7,31 +7,50 @@ import ThemeToggle from "./theme-toggler";
 
 export default function Navbar() {
     return (
-        <nav className={styles.navbar}>
-            <div className={styles.buttons}>
-                <Button href="/">Home</Button>
-                <Button href="/about">About</Button>
-                <Button href="/projects">Projects</Button>
-            </div>
-            <Link href="/">
-                <Image
-                    src="/images/logo/logo-dark.png"
-                    alt="Logo"
-                    width={172}
-                    height={31}
-                    className={styles.logo}
-                    priority={true}
-                />
-            </Link>
-            <div className={`${styles.buttons} ${styles.right}`}>
-                <Button href="/contacts">Contacts</Button>
-                {/* <Link href="/" locale="it">
-                    <Button className={styles.icon} animation={false}>
-                        <Globe size={20} />
-                    </Button>
-                </Link> */}
-                <ThemeToggle styles={styles} />
-            </div>
-        </nav>
+        <>
+            <nav className={`${styles.navbar} onlyDesktop`}>
+                <div className={styles.buttons}>
+                    <Button href="/">Home</Button>
+                    <Button href="/about">About</Button>
+                    <Button href="/projects">Projects</Button>
+                </div>
+                <Link href="/">
+                    <Image
+                        src="/images/logo/logo-dark.png"
+                        alt="Logo"
+                        width={172}
+                        height={31}
+                        className={styles.logo}
+                        priority={true}
+                    />
+                </Link>
+                <div className={`${styles.buttons} ${styles.right}`}>
+                    <Button href="/contacts">Contacts</Button>
+                    <ThemeToggle styles={styles} />
+                </div>
+            </nav>
+
+            <nav className={`${styles.navbar} onlyMobile`}>
+                <div className={`${styles.buttons} ${styles.themeToggle}`}>
+                    <ThemeToggle styles={styles} />
+                </div>
+                <Link href="/">
+                    <Image
+                        src="/images/logo/icona-dark.png"
+                        alt="Logo"
+                        width={40}
+                        height={40}
+                        className={styles.logo}
+                        priority={true}
+                    />
+                </Link>
+                <div className={`${styles.buttons} ${styles.right}`}>
+                    <button className={styles.menuIcon}>
+                        <div className={styles.bar}></div>
+                        <div className={styles.bar}></div>
+                    </button>
+                </div>
+            </nav>
+        </>
     );
 }
