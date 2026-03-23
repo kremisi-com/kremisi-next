cd3d92650f0fdfffc6c4d6ef04fa00122e8d6a74
Napolitano Andrea <napolitanoandrea00@gmail.com>
2026-03-23 21:11:27 +0100

    creazione overview

--- src/components/overview/overview.jsx
+++ src/components/overview/overview.jsx
@@ -1,9 +1,21 @@
+import RevelatingText from "@/components/revelating-text/revelating-text";
 import styles from "./overview.module.css";
 
 export default function Overview() {
     return (
-        <div className={styles.overview}>
-            <h1>Overview</h1>
-        </div>
+        <section className={styles.overview} id="overview">
+            <div className={styles.background}>
+                <div className={styles.blob1}></div>
+                <div className={styles.blob2}></div>
+                <div className={styles.blob3}></div>
+                <div className={styles.noise}></div>
+            </div>
+            
+            <div className={styles.content}>
+                <RevelatingText>
+                    We shape the <span className="highlight">future</span><br/>through uncompromising <span className="highlight">digital design.</span>
+                </RevelatingText>
+            </div>
+        </section>
     );
 }
