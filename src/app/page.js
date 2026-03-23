5beec8f4776a49aacd66c60f0844cd9c856a1b97
Napolitano Andrea <napolitanoandrea00@gmail.com>
2026-03-23 10:57:48 +0100

    rimosse cose inutili ai gen

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import styles from "./page.module.css";
 import MainSlider from "@/components/main-slider/main-slider";
-import IntroSEO from "@/components/intro-seo/intro-seo";
+
 import {
   getProjectsArray,
   getOrganizedProjects,
@@ -22,7 +22,6 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <MainSlider projectsData={sortedProjects} />
-      {/* <IntroSEO /> */}
     </div>
   );
 }
